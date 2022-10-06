c6318935b1a3324cad1f93936d8f2eb9301c1ff3
JonasNaumannSWO <114401078+JonasNaumannSWO@users.noreply.github.com>
2022-10-06 15:08:47 +0200

    Delete resources directory

--- resources/javascript/about-me.js
+++
@@ -1,7 +0,0 @@
-//adjust playing speed of background
-document.getElementById("backgroundVideo").playbackRate = 0.6;
-
-//urlTab input links
-document.getElementById("urlTabHome").onclick = function () {
-  window.open("/index.html", "_self");
-};
--- resources/javascript/footer.js
+++
@@ -1,3 +0,0 @@
-//place footer at bottom based on body size
-document.getElementById("footer").style.marginTop =
-  window.innerHeight - document.body.clientHeight + 15 + "px";
